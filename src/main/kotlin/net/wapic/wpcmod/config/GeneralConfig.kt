de8e8594ab4843178796c947817be686b56dda14
Wapic <17051068+Wapic@users.noreply.github.com>
2025-07-07 04:25:01 +0200

    add button to open Shortcut Editor from config

--- src/main/kotlin/net/wapic/wpcmod/config/GeneralConfig.kt
+++ src/main/kotlin/net/wapic/wpcmod/config/GeneralConfig.kt
@@ -2,7 +2,10 @@ package net.wapic.wpcmod.config
 
 import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorButton
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
+import net.minecraft.client.MinecraftClient
+import net.wapic.wpcmod.general.shortcut.ShortcutScreen
 
 class GeneralConfig {
 
@@ -11,4 +14,12 @@ class GeneralConfig {
     @ConfigOption(name = "Fullbright", desc = "Makes things not dark")
     @ConfigEditorBoolean
     var fullbright: Boolean = false
+
+    @Expose
+    @JvmField
+    @ConfigOption(name = "Command Keybind Editor", desc = "Opens the screen to manage command keybinds")
+    @ConfigEditorButton(buttonText = "Open")
+    val runnable = Runnable {
+        MinecraftClient.getInstance().setScreen(ShortcutScreen())
+    }
 }
