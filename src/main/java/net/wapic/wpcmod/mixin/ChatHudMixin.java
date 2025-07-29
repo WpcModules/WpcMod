aefca3db1ccca2ef00e18e71be347c12fb8126b4
Wapic <17051068+Wapic@users.noreply.github.com>
2025-07-29 14:06:16 +0200

    feat: Compact Chat (#21)
    * refactor: move SpamFilter to new package
    
    * feat: Compact Chat
    
    * refactor: remove redundant semi colons

--- src/main/java/net/wapic/wpcmod/mixin/ChatHudMixin.java
+++ src/main/java/net/wapic/wpcmod/mixin/ChatHudMixin.java
@@ -1,15 +1,49 @@
 package net.wapic.wpcmod.mixin;
 
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.ChatHudLine;
+import net.minecraft.text.Text;
 import net.wapic.wpcmod.WpcMod;
+import net.wapic.wpcmod.features.chat.CompactChat;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChatHud.class)
-public class ChatHudMixin {
+import java.util.List;
+
+
+@Mixin(value = ChatHud.class, priority = Integer.MAX_VALUE)
+public abstract class ChatHudMixin {
+
+	@Shadow
+	@Final
+	private List<ChatHudLine> messages;
+
+	@Shadow
+	protected abstract void refresh();
+
 	@ModifyConstant(method = {"addMessage(Lnet/minecraft/client/gui/hud/ChatHudLine;)V", "addVisibleMessage"}, constant = @Constant(intValue = 100))
 	private int injected(int value) {
 		return (int) WpcMod.config.getChatConfig().getChatHistoryLength();
+	}
+
+	@ModifyVariable(
+			method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/client/gui/hud/MessageIndicator;)V",
+			at = @At("HEAD"),
+			argsOnly = true
+	)
+	public Text addMessage(final Text message) {
+		Text text = CompactChat.INSTANCE.compactMessage(message, messages);
+		if (!text.getString().equals(message.getString())) {
+			this.refresh();
+		}
+		return text;
+	}
+
+	@Inject(method = "clear", at = @At("HEAD"))
+	public void clear(CallbackInfo ci) {
+		CompactChat.INSTANCE.clear();
 	}
 }
