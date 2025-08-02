4c06151384ad150ac83af02e3d1772ea30771adc
Wapic <17051068+Wapic@users.noreply.github.com>
2025-08-02 11:48:04 +0200

    fix: config directories not being created on save

--- src/main/kotlin/net/wapic/wpcmod/config/ConfigManager.kt
+++ src/main/kotlin/net/wapic/wpcmod/config/ConfigManager.kt
@@ -69,6 +69,9 @@ object ConfigManager {
 
 	fun saveConfig() {
 		try {
+			if (!WpcMod.configDir.exists()) {
+				WpcMod.configDir.mkdirs()
+			}
 			WpcMod.logger.info("Saving config file")
 			tempFile.writeText(gson.toJson(jsonHolder))
 			if (file.exists()) file.copyTo(backupFile, overwrite = true)
