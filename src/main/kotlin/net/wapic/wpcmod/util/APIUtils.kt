2c54e74003e0f6722a7e75ee924b81405878b1f4
Wapic <17051068+Wapic@users.noreply.github.com>
2025-10-18 21:41:33 +0200

    fix Paul bonus not working when he is minister

--- src/main/kotlin/net/wapic/wpcmod/util/APIUtils.kt
+++ src/main/kotlin/net/wapic/wpcmod/util/APIUtils.kt
@@ -21,12 +21,19 @@ object APIUtils {
 		val response = fetch("https://api.hypixel.net/resources/skyblock/election") ?: return false
 		val jsonObject = JsonParser.parseString(response).toJsonObject() ?: return false
 		if (jsonObject.getJsonPrimitive("success")?.asBoolean == true) {
+
 			val mayor = jsonObject.getJsonObject("mayor") ?: return false
+			val minister = mayor.getJsonObject("minister") ?: return false
+
 			val name = mayor.getJsonPrimitive("name")?.asString
+			val ministerName = minister.getJsonPrimitive("name")?.asString
+
 			if (name == "Paul") {
 				return mayor.getJsonArray("perks")?.any {
 					it.toJsonObject()?.getJsonPrimitive("name")?.asString == "EZPZ"
 				} ?: false
+			} else if (ministerName == "Paul") {
+				return minister.getAsJsonObject("perk")?.getJsonPrimitive("name")?.asString == "EZPZ"
 			}
 		}
 		return false
