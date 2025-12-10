36ae8d65f39af967a0cbb19edfd814ab22b9db75
Wapic <17051068+Wapic@users.noreply.github.com>
2025-12-10 22:19:58 +0100

    feat: Rend announce & custom gfs command(#53)
    * feat: fill GFS command

--- src/main/kotlin/net/wapic/wpcmod/WpcMod.kt
+++ src/main/kotlin/net/wapic/wpcmod/WpcMod.kt
@@ -50,6 +50,7 @@ import net.wapic.wpcmod.features.inventory.experiments.AutoExperiments
 import net.wapic.wpcmod.features.inventory.experiments.SuperpairsSolver
 import net.wapic.wpcmod.features.instance.CancelInteract
 import net.wapic.wpcmod.features.kuudra.KuudraESP
+import net.wapic.wpcmod.features.kuudra.RendAnnounce
 import net.wapic.wpcmod.features.mining.ChestESP
 import net.wapic.wpcmod.features.mining.PigeonSwapper
 import net.wapic.wpcmod.hud.HudManager
@@ -108,6 +109,7 @@ object WpcMod : ModInitializer {
 					.then(TermSimCommand.getCommand())
 					.then(FunnyMapCommands.getCommand())
 					.then(HudEditorCommand.getCommand())
+					.then(GFSCommand.getCommand())
 			)
 
 			dispatcher.register(ClientCommandManager.literal("wpcmod").redirect(mainCommand))
@@ -173,6 +175,7 @@ object WpcMod : ModInitializer {
 
 		// Kuudra
 		KuudraESP.init()
+		RendAnnounce.init()
 
 		// Galatea
 		GalateaESP.init()
---
+++ src/main/kotlin/net/wapic/wpcmod/features/kuudra/RendAnnounce.kt
@@ -0,0 +1,36 @@
+package net.wapic.wpcmod.features.kuudra
+
+import net.wapic.wpcmod.WpcMod
+import net.wapic.wpcmod.events.ServerTickEvent
+import net.wapic.wpcmod.events.WorldChangeEvent
+import net.wapic.wpcmod.util.ChatUtils
+import net.wapic.wpcmod.util.KuudraUtils
+import java.text.DecimalFormat
+
+object RendAnnounce {
+
+	private val config get() = WpcMod.config.kuudra
+	private var kuudraLastHP: Float = 25_000f
+
+	fun init() {
+		WorldChangeEvent.BEFORE.register { kuudraLastHP = 25_000f }
+
+		ServerTickEvent.EVENT.register(::onTick)
+	}
+
+	fun format(value: Float): String = DecimalFormat("#,###").format(value)
+
+	private fun onTick() {
+		if(!config.rendAnnounce) return
+		if(KuudraUtils.phase != KuudraUtils.Phase.KILL) return
+
+		KuudraUtils.kuudraEntity?.let {
+			val diff = kuudraLastHP - it.health
+			if(diff > 1666) {
+				ChatUtils.sendMessage("Someone pulled for: ${format(diff * 9600f)} damage")
+			}
+
+			kuudraLastHP = it.health
+		}
+	}
+}
