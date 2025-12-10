03c470862d2bf2f91e95c90d680983dbd07c8a15
Wapic <17051068+Wapic@users.noreply.github.com>
2025-12-10 07:05:52 +0100

    use camera position if freecam enabled to get room core

--- src/main/kotlin/net/wapic/wpcmod/commands/FunnyMapCommands.kt
+++ src/main/kotlin/net/wapic/wpcmod/commands/FunnyMapCommands.kt
@@ -9,30 +9,36 @@ import net.minecraft.ChatFormatting
 import net.wapic.wpcmod.features.dungeons.funnymap.dungeon.DungeonScan
 import net.wapic.wpcmod.features.dungeons.funnymap.dungeon.FunnyMap
 import net.wapic.wpcmod.features.dungeons.funnymap.dungeon.ScanUtils
+import net.wapic.wpcmod.features.general.Freecam
 import net.wapic.wpcmod.util.ChatUtils
 import net.wapic.wpcmod.util.MC
 import net.wapic.wpcmod.util.Utils
 
 object FunnyMapCommands : Command("dungeon") {
 
-	val getRoomData: LiteralArgumentBuilder<FabricClientCommandSource> = literal("room").executes{
-		val player = MC.player?.position() ?: return@executes 0
-		val pos = ScanUtils.getRoomCentre(player.x.toInt(), player.z.toInt())
-		val data = ScanUtils.getRoomData(pos.first, pos.second)
+	val roomDataFromPlayer: LiteralArgumentBuilder<FabricClientCommandSource> = literal("room").executes{
+		val pos = if(Freecam.isEnabled) {
+			MC.instance.cameraEntity?.position() ?: return@executes 0
+		} else {
+			MC.player?.position() ?: return@executes 0
+		}
+
+		val roomCentre = ScanUtils.getRoomCentre(pos.x.toInt(), pos.y.toInt())
+		val data = ScanUtils.getRoomData(roomCentre.first, roomCentre.second)
 		if (data != null) {
 			Utils.copyToClipboard(data.toString())
 			ChatUtils.sendMessage("Copied room data to clipboard.", Style.EMPTY.withColor(ChatFormatting.GREEN))
 		} else {
-			Utils.copyToClipboard("${ScanUtils.getCore(pos.first, pos.second)}")
+			Utils.copyToClipboard("${ScanUtils.getCore(roomCentre.first, roomCentre.second)}")
 			ChatUtils.sendMessage("§cExisting room data not found. §aCopied room core to clipboard.")
 		}
-	0
+		return@executes 0
 	}
 
 	val scan: LiteralArgumentBuilder<FabricClientCommandSource> = literal("scan").executes {
 		FunnyMap.reset()
 		DungeonScan.scan()
-		0
+		return@executes 0
 	}
 
 	override fun executes(context: CommandContext<FabricClientCommandSource>): Int {
@@ -41,6 +47,6 @@ object FunnyMapCommands : Command("dungeon") {
 	}
 
 	override fun getCommand(): LiteralArgumentBuilder<FabricClientCommandSource> {
-		return command.then(getRoomData).then(scan)
+		return command.then(roomDataFromPlayer).then(scan)
 	}
 }
