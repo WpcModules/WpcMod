d2f68b5524488861a75fce2b205afb7886afa746
Wapic <17051068+Wapic@users.noreply.github.com>
2026-02-01 15:33:21 +0100

    check headtexture of potential mimic before setting mimic dead

--- src/main/kotlin/net/wapic/wpcmod/features/dungeons/ScoreCalculation.kt
+++ src/main/kotlin/net/wapic/wpcmod/features/dungeons/ScoreCalculation.kt
@@ -256,7 +256,7 @@ object ScoreCalculation : SimpleHudElement("Score Calculation", 140, 160) {
 		val playerDistanceFromMimic = client.player?.distanceToSqr(mimicPos?.center ?: return) ?: return
 		if (playerDistanceFromMimic >= 400.0) return
 
-		val isMimicDead = client.level?.entitiesForRendering()?.none { it is Zombie && it.isBaby }
+		val isMimicDead = client.level?.entitiesForRendering()?.none { it is Zombie && it.isBaby && it.headTexture == HeadTextures.MIMIC }
 		if (isMimicDead == true)
 			setMimicDead(config.mimicMessage)
 	}
