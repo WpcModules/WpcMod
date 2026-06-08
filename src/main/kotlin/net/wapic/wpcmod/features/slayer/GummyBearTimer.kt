12d57977b1a069193a854a89b9b970ed4eeec842
ValsCSGO <valscsgo@gmail.com>
2026-06-08 19:57:31 -0400

    Fix: gummy bear timer never resetting when timer ends

--- src/main/kotlin/net/wapic/wpcmod/features/slayer/GummyBearTimer.kt
+++ src/main/kotlin/net/wapic/wpcmod/features/slayer/GummyBearTimer.kt
@@ -37,7 +37,7 @@ object GummyBearTimer : SimpleHudElement("Gummy Bear Timer", 110, 11) {
 
 		val timeLeft = ((endTime - System.currentTimeMillis()) / 1000).toInt()
 		if (timeLeft < 0) {
-			if (endTime.toInt() != -1) endTime = -1; //might as well not set it infinite times, idk if the check really helps
+			if (endTime.toInt() != -1) endTime = -1;
 			if (!config.gummyBearTimer.showExpired) return
 		}
 
