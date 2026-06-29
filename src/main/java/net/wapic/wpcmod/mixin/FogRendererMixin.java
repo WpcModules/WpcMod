1382a52725beccab1035959264f2fc10a033fde9
Wapic <17051068+Wapic@users.noreply.github.com>
2026-06-29 19:39:36 +0200

    fix: no blindness crashing and add custom blindness modifier

--- src/main/java/net/wapic/wpcmod/mixin/FogRendererMixin.java
+++ src/main/java/net/wapic/wpcmod/mixin/FogRendererMixin.java
@@ -1,29 +1,28 @@
 package net.wapic.wpcmod.mixin;
 
-import net.minecraft.client.Camera;
-import net.minecraft.client.DeltaTracker;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.fog.FogData;
 import net.minecraft.client.renderer.fog.FogRenderer;
 import net.minecraft.world.effect.MobEffects;
 import net.wapic.wpcmod.WpcMod;
-import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FogRenderer.class)
 public class FogRendererMixin {
 
-	@Inject(at = @At("HEAD"), method = "setupFog", cancellable = true)
-	private static void applyFog(Camera camera, int renderDistanceInChunks, DeltaTracker deltaTracker, float darkenWorldAmount, ClientLevel level, CallbackInfoReturnable<Vector4f> cir) {
+	@ModifyReturnValue(method = "setupFog", at = @At("TAIL"))
+	private static FogData setupFog(FogData original) {
 		Minecraft client = Minecraft.getInstance();
-		if (client.player != null) {
+		float customBlindnessOpacity = WpcMod.config.getRender().getBlindnessOpacity();
+		if (client.player != null && customBlindnessOpacity < 1.0f) {
 			boolean isBlind = client.player.hasEffect(MobEffects.BLINDNESS);
-			if (WpcMod.config.getRender().getNoBlindness() && isBlind) {
-				cir.setReturnValue(new Vector4f(1f, 1f, 1f, 0f));
+			if (isBlind) {
+				original.color.w = customBlindnessOpacity;
+				return original;
 			}
 		}
+		return original;
 	}
 }
--- src/main/kotlin/net/wapic/wpcmod/config/render/RenderConfig.kt
+++ src/main/kotlin/net/wapic/wpcmod/config/render/RenderConfig.kt
@@ -1,7 +1,8 @@
 package net.wapic.wpcmod.config.render
 
-import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorSlider
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 
 class RenderConfig {
 
@@ -9,9 +10,12 @@ class RenderConfig {
 	@ConfigEditorBoolean
 	var fullbright: Boolean = false
 
-	@ConfigOption(name = "No Blindness", desc = "Makes things not dark when its really dark")
-	@ConfigEditorBoolean
-	var noBlindness: Boolean = false
+	@ConfigOption(
+		name = "Blindness Opacity",
+		desc = "Set the opacity of blindness, 0.0 = no blindness, 1.0 = normal blindness"
+	)
+	@ConfigEditorSlider(minValue = 0.0f, maxValue = 1.0f, minStep = 0.1f)
+	var blindnessOpacity: Float = 1.0f
 
 	@ConfigOption(name = "Disable Fluid Fog", desc = "Disable fog in fluids")
 	@ConfigEditorBoolean
