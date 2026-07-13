ce1424e2086149c3efbcc40f5147ad6f5b8bda76
Wapic <17051068+Wapic@users.noreply.github.com>
2026-07-13 18:19:56 +0200

    fix: TrapperAPI not resetting values on quest complete / warp

--- src/main/kotlin/net/wapic/wpcmod/util/TrapperAPI.kt
+++ src/main/kotlin/net/wapic/wpcmod/util/TrapperAPI.kt
@@ -3,12 +3,15 @@ package net.wapic.wpcmod.util
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents
 import net.minecraft.network.chat.Component
 import net.wapic.wpcmod.WpcMod
+import net.wapic.wpcmod.events.WorldChangeEvent
 import net.wapic.wpcmod.util.ChatUtils.removeFormatting
 
 object TrapperAPI {
 
 	private val TRAPPER_QUEST_MESSAGE =
 		Regex("^\\[NPC] Trevor: You can find your (?<mobType>(?:UN)?(?:TRACKABLE|DETECTED)|E(?:LUSIVE|NDANGERED)) animal near the (?<area>.*)\\.$")
+
+	private val TRAPPER_MOB_KILLED_MESSAGE = Regex("^Killing the animal rewarded you \\d+ pelts.$")
 
 	var currentType: TrapperType? = null
 		private set
@@ -24,10 +27,22 @@ object TrapperAPI {
 
 	fun init() {
 		ClientReceiveMessageEvents.GAME.register(::onMessageReceived)
+		WorldChangeEvent.AFTER.register { reset() }
+	}
+
+	fun reset() {
+		currentType = null
+		currentArea = null
+		WpcMod.LOGGER.debug("TrapperAPI: reset currentType & currentArea")
 	}
 
 	fun onMessageReceived(message: Component, isActionBar: Boolean) {
 		if (isActionBar) return
+
+		if (TRAPPER_MOB_KILLED_MESSAGE.matches(message.string)) {
+			reset()
+			return
+		}
 
 		val matcher = TRAPPER_QUEST_MESSAGE.matchEntire(message.string.removeFormatting()) ?: return
 		val mobType =
