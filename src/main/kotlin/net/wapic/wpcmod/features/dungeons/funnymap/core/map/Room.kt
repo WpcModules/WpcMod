8540c29b83fa7fba8fbf0fd9d63ca9dfbe5755a9
Wapic <17051068+Wapic@users.noreply.github.com>
2026-09-01 17:12:58 +0200

    fix: Mimic Room showing when Legit Mode is active

--- src/main/kotlin/net/wapic/wpcmod/features/dungeons/funnymap/core/map/Room.kt
+++ src/main/kotlin/net/wapic/wpcmod/features/dungeons/funnymap/core/map/Room.kt
@@ -6,21 +6,25 @@ import net.wapic.wpcmod.features.dungeons.funnymap.ui.MapElement
 import java.awt.Color
 
 class Room(override val x: Int, override val z: Int, var data: RoomData) : Tile {
-	private val config get() = WpcMod.config.dungeon.funnyMap.colors
+
+	private val config get() = WpcMod.config.dungeon.funnyMap
+	private val colors get() = WpcMod.config.dungeon.funnyMap.colors
+
 	var core = 0
 	var isSeparator = false
 	var uniqueRoom: UniqueRoom? = null
+
 	override var state: RoomState = RoomState.UNDISCOVERED
 	override val color: Color
-		get() = if (MapElement.legitRender && state == RoomState.UNOPENED) config.colorUnopened.getEffectiveColour()
+		get() = if (MapElement.legitRender && state == RoomState.UNOPENED) colors.colorUnopened.getEffectiveColour()
 		else when (data.type) {
-			RoomType.BLOOD -> config.colorBlood.getEffectiveColour()
-			RoomType.CHAMPION -> config.colorMiniboss.getEffectiveColour()
-			RoomType.ENTRANCE -> config.colorEntrance.getEffectiveColour()
-			RoomType.FAIRY -> config.colorFairy.getEffectiveColour()
-			RoomType.PUZZLE -> config.colorPuzzle.getEffectiveColour()
-			RoomType.RARE -> config.colorRare.getEffectiveColour()
-			RoomType.TRAP -> config.colorTrap.getEffectiveColour()
-			else -> if (uniqueRoom?.hasMimic == true) config.colorRoomMimic.getEffectiveColour() else config.colorRoom.getEffectiveColour()
+			RoomType.BLOOD -> colors.colorBlood.getEffectiveColour()
+			RoomType.CHAMPION -> colors.colorMiniboss.getEffectiveColour()
+			RoomType.ENTRANCE -> colors.colorEntrance.getEffectiveColour()
+			RoomType.FAIRY -> colors.colorFairy.getEffectiveColour()
+			RoomType.PUZZLE -> colors.colorPuzzle.getEffectiveColour()
+			RoomType.RARE -> colors.colorRare.getEffectiveColour()
+			RoomType.TRAP -> colors.colorTrap.getEffectiveColour()
+			else -> if (uniqueRoom?.hasMimic == true && !config.legitMode) colors.colorRoomMimic.getEffectiveColour() else colors.colorRoom.getEffectiveColour()
 		}
 }
